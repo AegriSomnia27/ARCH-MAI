0612cb001ec7da46a558583a54feab5338a150c4
Dmitry Ryabov <ksourdes@gmail.com>
2022-04-13 21:34:18 +0000

    2nd assignment

---
+++ 1.REST_Server/build/CMakeFiles/Rest_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rest_server.dir/config/config.cpp.o"
+  "CMakeFiles/Rest_server.dir/database/database.cpp.o"
+  "CMakeFiles/Rest_server.dir/database/person.cpp.o"
+  "CMakeFiles/Rest_server.dir/main.cpp.o"
+  "Rest_server"
+  "Rest_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rest_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
